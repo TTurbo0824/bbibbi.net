c4f0c29b830d5b969bc53c390338dd62f84a2e13
Kyungjoo Ha <429why@gmail.com>
2022-02-18 23:23:05 +0900

    modify: Webpack config

--- with-webpack/webpack.config.js
+++ with-webpack/webpack.config.js
@@ -1,12 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    main: './src/index.js'
+    index: './src/index.js',
+    about: './src/about.js',
   },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].js'
+    filename: '[name]_bundle.js',
   },
-}
+  module: {
+    rules: [{test: /\.css/i, use: ['style-loader', 'css-loader']}],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      filename: './about.html',
+      chunks: ['about'],
+    }),
+  ],
+};
