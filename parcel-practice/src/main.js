2fc790ab28886491134cbc57ecfce27324baa999
Kyungjoo Ha <429why@gmail.com>
2022-02-19 16:10:13 +0900

    add: Source files

---
+++ parcel-practice/src/main.js
@@ -0,0 +1,8 @@
+// import './main.scss';
+import sub from './sub';
+
+const h2 = document.createElement('h2');
+h2.innerText = 'Hello Parcel';
+document.body.appendChild(h2);
+
+sub('call sub');
---
+++ parcel-practice/src/sub.js
@@ -0,0 +1,7 @@
+const sub = (str) => {
+  const h2 = document.createElement('h2');
+  h2.innerText = str;
+  document.body.appendChild(h2);
+};
+
+export default sub;
