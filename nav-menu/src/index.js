299bbbf598140f2cd8c59fafd4d953f943a0b162
Kyungjoo Ha <429why@gmail.com>
2022-03-24 22:27:44 +0900

    modify: File

--- nav-menu/src/index.js
+++ nav-menu/src/index.js
@@ -11,38 +11,76 @@ const menu = [
   {title: 'Echo', route: '/echo', id: 'echo'},
 ];
 
+const navBar = create('div', 'nav-bar');
 const navContainer = create('nav', 'nav-container');
-menu.forEach(
-  (el) => {
-    const navItem = create('span', 'nav-item');
-    navItem.innerHTML = el.title;
-    navContainer.append(navItem)
-  }
-);
 
-root.append(navContainer);
-let navUnderline = document.getElementById('nav-underline');
+menu.forEach((el) => {
+  const navItem = create('span', 'nav-item');
+  navItem.id = el.id;
+  navItem.innerHTML = el.title;
+  navContainer.append(navItem);
+});
+
+const clickNav = (e) => {
+  console.log(e.target.innerHTML);
+};
+
+navContainer.addEventListener('click', (e) => clickNav(e));
+const navUnderline = create('div', 'nav-underline');
+navContainer.append(navUnderline);
+navBar.append(navContainer);
+root.append(navBar);
+
 let navItems = document.querySelectorAll('nav span');
-let target = document.getElementById(currentLocation);
-let navB = document.getElementByC('nav-bar');
-window.addEventListener('load', () => findTarget());
-window.addEventListener('resize', () => findTarget());
+
+// let target = document.getElementById(currentLocation);
+// let navB = document.getElementById('nav-bar');
+// console.log(navB)
+
+// window.addEventListener('load', () => findTarget());
+// window.addEventListener('resize', () => findTarget());
 navItems.forEach((menu) =>
   menu.addEventListener('mouseover', (e) => hovering(e))
 );
-navItems.forEach((menu) =>
-  menu.addEventListener('onclick', () => findTarget())
-);
-navB && navB.addEventListener('mouseleave', () => findTarget());
 
-const findTarget = () => {
-  navUnderline &&
-    target &&
-    (navUnderline.style.left = target.offsetLeft + 'px');
-  navUnderline &&
-    target &&
-    (navUnderline.style.width = target.offsetWidth + 'px');
+const content = create('div', 'content');
+
+root.append(content);
+
+window.addEventListener('load', () => getPageContent());
+
+const getPageContent = (page) => {
+  let contentToReturn;
+  switch (page) {
+    case 'Alpha':
+      contentToReturn = menu[0].route;
+    case 'Bravo':
+      contentToReturn = menu[1].route;
+    case 'Charlie':
+      contentToReturn = menu[2].route;
+    case 'Delta':
+      contentToReturn = menu[3].route;
+    case 'Echo':
+      contentToReturn = menu[4].route;
+    default:
+      contentToReturn = menu[0].route;
+  }
+  content.innerHTML = contentToReturn;
 };
+
+// navItems.forEach((menu) =>
+//   menu.addEventListener('onclick', () => findTarget())
+// );
+// navB && navB.addEventListener('mouseleave', () => findTarget());
+
+// const findTarget = () => {
+//   navUnderline &&
+//     target &&
+//     (navUnderline.style.left = target.offsetLeft + 'px');
+//   navUnderline &&
+//     target &&
+//     (navUnderline.style.width = target.offsetWidth + 'px');
+// };
 
 const hovering = (e) => {
   navUnderline && (navUnderline.style.left = e.currentTarget.offsetLeft + 'px');
