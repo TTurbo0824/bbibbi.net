c4a6dae1e8488d78ecb3c355576f5c77fe1ab35b
Kyungjoo Ha <429why@gmail.com>
2022-03-25 15:48:05 +0900

    add: Router

--- nav-menu/src/index.js
+++ nav-menu/src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import {createElementWithClass as create} from './utilities';
 
+const currentLocation = window.location.pathname.split('/')[1];
 const root = document.querySelector('#root');
 
 const menu = [
@@ -21,66 +22,85 @@ menu.forEach((el) => {
   navContainer.append(navItem);
 });
 
-const clickNav = (e) => {
-  console.log(e.target.innerHTML);
+const clickNavItem = (e) => {
+  if (e.target.innerHTML.length < 10) {
+    getPageContent(e.target.innerHTML);
+    target = document.getElementById(window.location.pathname.split('/')[1]);
+  }
 };
 
-navContainer.addEventListener('click', (e) => clickNav(e));
+navContainer.addEventListener('click', (e) => clickNavItem(e));
 const navUnderline = create('div', 'nav-underline');
 navContainer.append(navUnderline);
 navBar.append(navContainer);
 root.append(navBar);
 
 let navItems = document.querySelectorAll('nav span');
+let target = document.getElementById(currentLocation);
 
-// let target = document.getElementById(currentLocation);
-// let navB = document.getElementById('nav-bar');
-// console.log(navB)
-
-// window.addEventListener('load', () => findTarget());
-// window.addEventListener('resize', () => findTarget());
 navItems.forEach((menu) =>
   menu.addEventListener('mouseover', (e) => hovering(e))
 );
 
 const content = create('div', 'content');
-
+content.id = 'content';
 root.append(content);
 
+window.addEventListener('load', () => findTarget());
 window.addEventListener('load', () => getPageContent());
 
 const getPageContent = (page) => {
   let contentToReturn;
   switch (page) {
     case 'Alpha':
-      contentToReturn = menu[0].route;
+      historyRouterPush(menu[0].route);
+      contentToReturn = page;
+      break;
     case 'Bravo':
-      contentToReturn = menu[1].route;
+      historyRouterPush(menu[1].route);
+      contentToReturn = page;
+      break;
     case 'Charlie':
-      contentToReturn = menu[2].route;
+      historyRouterPush(menu[2].route);
+      contentToReturn = page;
+      break;
     case 'Delta':
-      contentToReturn = menu[3].route;
+      historyRouterPush(menu[3].route);
+      contentToReturn = page;
+      break;
     case 'Echo':
-      contentToReturn = menu[4].route;
+      historyRouterPush(menu[4].route);
+      contentToReturn = page;
+      break;
     default:
-      contentToReturn = menu[0].route;
+      let routeIdx;
+      menu.forEach((el, idx) => {
+        if (window.location.pathname === el.route) routeIdx = idx
+      })
+      historyRouterPush(menu[routeIdx].route);
+      contentToReturn = menu[routeIdx].title;
+      break;
   }
-  content.innerHTML = contentToReturn;
+  document.getElementById('content').innerHTML = contentToReturn;
+};
+
+const historyRouterPush = (pathName) => {
+  window.history.pushState({}, pathName, window.location.origin + pathName);
 };
 
 // navItems.forEach((menu) =>
-//   menu.addEventListener('onclick', () => findTarget())
+//   menu.addEventListener('click', () => findTarget())
 // );
-// navB && navB.addEventListener('mouseleave', () => findTarget());
+navBar && navBar.addEventListener('mouseleave', () => findTarget());
 
-// const findTarget = () => {
-//   navUnderline &&
-//     target &&
-//     (navUnderline.style.left = target.offsetLeft + 'px');
-//   navUnderline &&
-//     target &&
-//     (navUnderline.style.width = target.offsetWidth + 'px');
-// };
+const findTarget = () => {
+  navUnderline &&
+    target &&
+    (navUnderline.style.left = target.offsetLeft + 'px');
+  navUnderline &&
+    target &&
+    (navUnderline.style.width = target.offsetWidth + 'px');
+};
 
 const hovering = (e) => {
   navUnderline && (navUnderline.style.left = e.currentTarget.offsetLeft + 'px');
